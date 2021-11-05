ec38e86954765aeeef1b0233afd0154a88694e2c
Penguin-12 <f20190824@pilani.bits-pilani.ac.in>
2021-11-05 15:19:54 +0530

    More changes

--- lib/src/nice_client.dart
+++ lib/src/nice_client.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:meta/meta.dart';
 
-class NiceClient extends BaseClient {
+
+class NiceClient  {
   NiceClient({
     Client client,
     @required this.baseUrl,
@@ -14,42 +15,35 @@ class NiceClient extends BaseClient {
   final String baseUrl;
   final Map<String, String> headers;
 
-  @override
+
   Future<Response> head(String url, {Map<String, String> headers}) {
-    return super.head(Uri.parse('$baseUrl$url'), headers: headers);
+    return _inner.head(Uri.parse('$baseUrl$url'), headers: headers);
   }
 
-  @override
   Future<Response> get(String url, {Map<String, String> headers}) {
-    return super.get(Uri.parse('$baseUrl$url'), headers: headers);
+    return _inner.get(Uri.parse('$baseUrl$url'), headers: headers);
   }
 
-  @override
   Future<Response> post(String url,
       {Map<String, String> headers, body, Encoding encoding}) {
-    return super
-        .post(Uri.parse('$baseUrl$url'), headers: headers, body: body, encoding: encoding);
+    return _inner.post(Uri.parse('$baseUrl$url'), headers: headers, body: body, encoding: encoding);
   }
 
-  @override
   Future<Response> put(String url,
       {Map<String, String> headers, body, Encoding encoding}) {
-    return super
-        .put(Uri.parse('$baseUrl$url'), headers: headers, body: body, encoding: encoding);
+    return _inner.put(Uri.parse('$baseUrl$url'), headers: headers, body: body, encoding: encoding);
   }
 
-  @override
   Future<Response> patch(String url,
       {Map<String, String> headers, body, Encoding encoding}) {
-    return super.patch(Uri.parse('$baseUrl$url'),
+    return _inner.patch(Uri.parse('$baseUrl$url'),
         headers: headers, body: body, encoding: encoding);
   }
 
   Future<Response> delete(String url, {Map<String, String> headers}) {
-    return super.delete(Uri.parse('$baseUrl$url'), headers: headers);
+    return _inner.delete(Uri.parse('$baseUrl$url'), headers: headers);
   }
 
-  @override
   Future<StreamedResponse> send(BaseRequest request) {
     request.headers.addAll(headers);
     return _inner.send(request);
